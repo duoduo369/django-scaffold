4dbc81fd8ca5297d09ee7f1a417d6f0ca3d2071f
duoduo369 <duoduo3369@gmail.com>
2014-11-15 15:54:45 +0800

    更新重命名脚本

---
+++ tools/rename_and_backup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+项目重命名并且备份
+'''
+
+import click
+import os
+from sh import cp, mv
+
+
+@click.command()
+@click.option('--newname', prompt='new project name', help='Please input new project name.')
+def rename(newname):
+    this_file_path = os.path.split(os.path.realpath(__file__))[0]
+    old_project_path = os.path.split(this_file_path)[0]
+    oldname = os.path.split(old_project_path)[-1]
+    new_project_path = os.path.join(os.path.split(old_project_path)[0], newname)
+    cp_cmd = cp('-r', old_project_path, new_project_path)
+    click.echo(''.join(cp_cmd))
+    django_settings_dir = os.path.join(new_project_path, oldname)
+    if os.path.exists(django_settings_dir):
+        mv(django_settings_dir, os.path.join(new_project_path, newname))
+    nginx_dev_conf = '{}/deploy/nginx/{}-dev.conf'
+    mv(nginx_dev_conf.format(new_project_path, oldname), nginx_dev_conf.format(new_project_path, newname))
+    cmd ='cd {} && grep {} -ril ./ | xargs sed -i "s/{}/{}/g"'.format(new_project_path, oldname, oldname, newname)
+    os.system(cmd)
+    click.echo('new files in {}'.format(new_project_path))
+    click.echo("please run cmd: 'cd {} && grep {} -ril ./' to check if all string has replaced!".format(new_project_path, oldname))
+    click.echo('done.')
+
+if __name__ == '__main__':
+    rename()

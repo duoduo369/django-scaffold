7f17669a887b151f1fbefa9dcdf4bd838291e487
duoduo369 <duoduo3369@gmail.com>
2014-11-17 22:02:31 +0800

    更新run_master.sh, 首先collect静态文件

--- tools/run_master.sh
+++ tools/run_master.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 case $1 in
     "start")
+        yes yes | python /opt/projects/django-scaffold/manage.py collectstatic
         if [ -e /run/django-scaffold-supervisord.pid ] ;then
           /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf restart django-scaffold
         else
@@ -15,6 +16,7 @@ case $1 in
         ;;
     "restart")
         if [ -e /run/django-scaffold-supervisord.pid ] ;then
+          yes yes | python /opt/projects/django-scaffold/manage.py collectstatic
           /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf restart django-scaffold
         else
           $0 start

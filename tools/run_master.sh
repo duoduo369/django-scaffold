0a6ca18a2d70452316660e40edf52ad282057e05
duoduo369 <duoduo3369@gmail.com>
2014-11-17 11:55:07 +0800

    添加master启动脚本

---
+++ tools/run_master.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+case $1 in
+    "start")
+        if [ -e /run/django-scaffold-supervisord.pid ] ;then
+          /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf restart django-scaffold
+        else
+          /opt/python_env/django-scaffold/bin/supervisord -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf
+        fi
+        exit ${ret}
+        ;;
+    "stop")
+        if [ -e /run/django-scaffold-supervisord.pid ] ;then
+          /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf stop django-scaffold && /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf shutdown
+        fi
+        ;;
+    "restart")
+        if [ -e /run/django-scaffold-supervisord.pid ] ;then
+          /opt/python_env/django-scaffold/bin/supervisorctl -c /opt/projects/django-scaffold/deploy/supervisor/supervisord.conf restart django-scaffold
+        else
+          $0 start
+        fi
+        ;;
+    *)
+        echo "usage: $(basename $0) {start|stop|restart}"
+        exit 1
+        ;;
+esac

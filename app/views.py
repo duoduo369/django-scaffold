ce709f1242b5abc081f0a53817ab208d0aefc1b7
duoduo369 <duoduo3369@gmail.com>
2014-11-17 15:44:35 +0800

    更新index的log，做log测试
    tail -f /var/log/django-scaffold/dev-stdout.log
    tail -f /var/log/django-scaffold/stderr.log
    tail -f /var/log/django-scaffold/ngerror.log
    tail -f /var/log/django-scaffold/ngaccess.log

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from djangomako.shortcuts import render_to_response
+import logging
 
+logger = logging.getLogger(__name__)
 
 def index(request):
+    logger.error('error log test')
+    logger.info('info log test')
+    logger.debug('debug log test')
     return render_to_response('index.html', {})

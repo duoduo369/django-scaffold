b36fc15aa11b2cb63837c5115932e7f32a716247
duoduo369 <duoduo3369@gmail.com>
2014-11-18 14:58:55 +0800

    add i18n config

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from djangomako.shortcuts import render_to_response
 import logging
+
+from django.utils.translation import ugettext as _
+from djangomako.shortcuts import render_to_response
 
 logger = logging.getLogger(__name__)
 
 def index(request):
-    logger.error('error log test')
-    logger.info('info log test')
-    logger.debug('debug log test')
+    logger.error(_('error log test'))
+    logger.info(_('info log test'))
+    logger.debug(_('debug log test'))
     return render_to_response('index.html', {})
--- django-scaffold/settings.py
+++ django-scaffold/settings.py
@@ -78,6 +78,10 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''

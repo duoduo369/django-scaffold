97d43f3fae3cde683970f010c96a98733ab6dbd5
duoduo369 <duoduo3369@gmail.com>
2014-11-20 00:46:23 +0800

    admin i18n

--- myauth/models.py
+++ myauth/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext as _
 
 
 class UserProfile(models.Model):
@@ -11,14 +12,16 @@ class UserProfile(models.Model):
     username: 用户名是唯一的，可以为Null
     nickname: 昵称是可以变的，可以重复
     '''
-    user = models.OneToOneField(User, unique=True, related_name='profile')
-    username = models.CharField(blank=True, null=True, unique=True, max_length=255)
-    nickname = models.CharField(blank=True, max_length=255, db_index=True, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(User, unique=True, related_name='profile', verbose_name=_('用户'))
+    username = models.CharField(blank=True, null=True, unique=True, max_length=255, verbose_name=_('用户名(唯一)'))
+    nickname = models.CharField(blank=True, max_length=255, db_index=True, default='', verbose_name=_('昵称(可变)'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('创建日期'))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('修改日期'))
 
     class Meta:
         db_table = "auth_userprofile"
+        verbose_name = _('用户附加信息')
+        verbose_name_plural = _('用户附加信息')
 
 
 @receiver(pre_save, sender=User)

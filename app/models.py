afcc092c09dbf3ed9b149d705bf68d3fc173abb8
duoduo369 <duoduo3369@gmail.com>
2014-11-20 17:14:31 +0800

    add south script

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext as _
 
 class App(models.Model):
     name = models.CharField(max_length=32)
+    url = models.CharField(max_length=255, default='')
     desc = models.TextField()
     created_user = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
